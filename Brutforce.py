ce431de6394b42e1ce20a3ea9eb35a2167a2c961
Mustafe <mustafeyare041@gmail.com>
2024-12-03 21:56:32 +0300

    Create Brutforce.py

---
+++ Brutforce.py
@@ -0,0 +1,17 @@
+_____      _            
+ |  __ \    (_)           
+ | |__) |_ _ _ _ __ __  __
+ |  ___/ _` | | '_ \\ \/ /
+ | |  | (_| | | | | |>  < 
+ |_|   \__,_|_|_| |_/_/\_\
+                          
+                          print("============================")
+print("💀Brute force attack-created By painx☠️")
+print("============================")
+#prompt for user inputp
+Id = input("enter target ID")
+
+name = input("Enter target username")
+wordlest = input("Enter wordlest file")
+print("starting Brute force attack")
+#read and process the wordlest while read-r password:
